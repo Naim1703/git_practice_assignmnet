c769bd398f6562627f5e92d0b4eebf6cb013a770
Naim1703 <naim.nmg90@gmail.com>
2022-12-01 16:58:23 +0530

    check

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let str = "abdba"
+
+let bag = "";
+for (let i=str.length-1; i>=0; i--)
+  {
+    bag+=str[i];
+  }
+if (str=bag)
+{
+  console.log("string is palindrome");
+}
+else
+{
+  console.log(" string is not a palindrome");
+}
---
+++ prime_numbers.js
@@ -0,0 +1,18 @@
+let input = 3;
+
+let count = 0;
+for (let i=1; i<=input; i++)
+  {
+    if (input%i==0)
+    {
+      count++;
+    }
+  }
+if (count==2)
+{
+  console.log("prime number");
+}
+else 
+{
+  console.log("not a prime number");
+}
